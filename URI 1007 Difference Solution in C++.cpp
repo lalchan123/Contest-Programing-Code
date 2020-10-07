d5800bf8a6a3086f2e09d4a2b3993a2a5820c1b6
lalchan123 <54817966+lalchan123@users.noreply.github.com>
2020-10-08 02:37:44 +0600

    Programming Contest Code

---
+++ URI 1007 Difference Solution in C++.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int A,B,C,D;
+    cin>>A>>B>>C>>D;
+    int dif=(A*B-C*D);
+    cout<<"DIFERENCA = "<<dif<<endl;
+
+}
---
+++ URI 1008 Salary Solution in C++.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,h;
+    float a;
+    cin>>n>>h;
+    cin>>a;
+    float salary=h*a;
+    cout<<"NUMBER = "<<n<<endl;
+    cout<<fixed;
+    cout<<setprecision(2)<<"SALARY = U$ "<<salary<<endl;
+
+}
