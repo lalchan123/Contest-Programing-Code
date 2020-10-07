f7dc147eb9a127ccf7a53dcba0e5f2227200a190
lalchan123 <54817966+lalchan123@users.noreply.github.com>
2020-10-08 02:17:24 +0600

    Programming Contest Code

---
+++ URI 1005 Average 1 Solution in C++.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  float A,B,med;
+  scanf("%f %f",&A,&B);
+  med=((A*3.5)+(B*7.5))/(3.5+7.5);
+  printf("MEDIA = %.5f\n",med);
+}
---
+++ URI 1006 Average 2 Solution in C++.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   float A,B,C,med;
+   scanf("%f %f %f",&A,&B,&C);
+   med=((A*2)+(B*3)+(C*5))/(2+3+5);
+   printf("MEDIA = %.1f\n",med);
+}
